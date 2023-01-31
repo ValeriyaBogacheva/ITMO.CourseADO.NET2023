3b048d397ae8b9f72799866339ccce5fbf65e462
ValeriyaBogacheva <117594694+ValeriyaBogacheva@users.noreply.github.com>
2023-01-31 20:41:53 +0300

    Add files via upload

---
+++ 9.1. CourseManager/CourseManager/CourseViewer.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace CourseManager
+{
+    partial class CourseViewer
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.departmentList = new System.Windows.Forms.ComboBox();
+            this.buttocloseFormn1 = new System.Windows.Forms.Button();
+            this.courseGridView = new System.Windows.Forms.DataGridView();
+            this.saveChanges = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.courseGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // departmentList
+            // 
+            this.departmentList.FormattingEnabled = true;
+            this.departmentList.Location = new System.Drawing.Point(13, 13);
+            this.departmentList.Name = "departmentList";
+            this.departmentList.Size = new System.Drawing.Size(121, 21);
+            this.departmentList.TabIndex = 0;
+            this.departmentList.SelectedIndexChanged += new System.EventHandler(this.departmentList_SelectedIndexChanged);
+            // 
+            // buttocloseFormn1
+            // 
+            this.buttocloseFormn1.Location = new System.Drawing.Point(141, 10);
+            this.buttocloseFormn1.Name = "buttocloseFormn1";
+            this.buttocloseFormn1.Size = new System.Drawing.Size(75, 23);
+            this.buttocloseFormn1.TabIndex = 1;
+            this.buttocloseFormn1.Text = "Закрыть";
+            this.buttocloseFormn1.UseVisualStyleBackColor = true;
+            this.buttocloseFormn1.Click += new System.EventHandler(this.buttocloseFormn1_Click);
+            // 
+            // courseGridView
+            // 
+            this.courseGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.courseGridView.Location = new System.Drawing.Point(13, 41);
+            this.courseGridView.Name = "courseGridView";
+            this.courseGridView.Size = new System.Drawing.Size(775, 397);
+            this.courseGridView.TabIndex = 2;
+            // 
+            // saveChanges
+            // 
+            this.saveChanges.Location = new System.Drawing.Point(222, 11);
+            this.saveChanges.Name = "saveChanges";
+            this.saveChanges.Size = new System.Drawing.Size(75, 23);
+            this.saveChanges.TabIndex = 1;
+            this.saveChanges.Text = "Обновить";
+            this.saveChanges.UseVisualStyleBackColor = true;
+            this.saveChanges.Click += new System.EventHandler(this.saveChanges_Click);
+            // 
+            // CourseViewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.courseGridView);
+            this.Controls.Add(this.saveChanges);
+            this.Controls.Add(this.buttocloseFormn1);
+            this.Controls.Add(this.departmentList);
+            this.Name = "CourseViewer";
+            this.Text = "Course Viewer";
+            this.Load += new System.EventHandler(this.CourseViewer_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.courseGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox departmentList;
+        private System.Windows.Forms.Button buttocloseFormn1;
+        private System.Windows.Forms.DataGridView courseGridView;
+        private System.Windows.Forms.Button saveChanges;
+    }
+}
+
---
+++ 9.1. CourseManager/CourseManager/CourseViewer.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Objects;
+using System.Data.Objects.DataClasses;
+
+
+
+namespace CourseManager
+{
+    public partial class CourseViewer : Form
+    {
+        private schoolEntities schoolContext;
+        public CourseViewer()
+        {
+            InitializeComponent();
+        }
+
+        private void CourseViewer_Load(object sender, EventArgs e)
+        {
+            schoolContext = new schoolEntities();
+            var departmentQuery = from d in schoolContext.Departments.Include("Courses")
+                                  orderby d.Name
+                                  select d;
+            try
+            {
+                this.departmentList.DisplayMember = "Name";
+                this.departmentList.DataSource = (departmentQuery.ToList());
+               
+                //((ObjectQuery)departmentQuery).Execute(MergeOption.AppendOnly);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+        }
+
+        private void departmentList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                Department department = (Department)this.departmentList.SelectedItem;
+
+                courseGridView.DataSource = department.Courses.ToList();
+
+                courseGridView.Columns["Department"].Visible = false;
+                courseGridView.Columns["StudentGrades"].Visible = false;
+                courseGridView.Columns["OnlineCourse"].Visible = false;
+                courseGridView.Columns["OnsiteCourse"].Visible = false;
+                courseGridView.Columns["People"].Visible = false;
+                courseGridView.Columns["DepartmentId"].Visible = false;
+
+                courseGridView.AllowUserToDeleteRows = false;
+                courseGridView.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void saveChanges_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                schoolContext.SaveChanges();
+                MessageBox.Show("Changes saved to the database.");
+                this.Refresh();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void buttocloseFormn1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            schoolContext.Dispose();
+
+        }
+    }
+}
