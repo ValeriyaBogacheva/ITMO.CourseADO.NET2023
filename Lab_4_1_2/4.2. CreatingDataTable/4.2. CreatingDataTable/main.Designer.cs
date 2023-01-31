81b3e58bc25d48f03ee26dc4b486d33ece7626a1
ValeriyaBogacheva <117594694+ValeriyaBogacheva@users.noreply.github.com>
2023-01-31 20:25:00 +0300

    Add files via upload

---
+++ Lab_4_1_2/4.2. CreatingDataTable/4.2. CreatingDataTable/main.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace CreatingDataTable
+{
+    partial class main
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AddRowButton = new System.Windows.Forms.Button();
+            this.TableGrid = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.TableGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // AddRowButton
+            // 
+            this.AddRowButton.Location = new System.Drawing.Point(13, 13);
+            this.AddRowButton.Name = "AddRowButton";
+            this.AddRowButton.Size = new System.Drawing.Size(75, 23);
+            this.AddRowButton.TabIndex = 0;
+            this.AddRowButton.Text = "Add Row";
+            this.AddRowButton.UseVisualStyleBackColor = true;
+            this.AddRowButton.Click += new System.EventHandler(this.AddRowButton_Click);
+            // 
+            // TableGrid
+            // 
+            this.TableGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.TableGrid.Location = new System.Drawing.Point(13, 43);
+            this.TableGrid.Name = "TableGrid";
+            this.TableGrid.Size = new System.Drawing.Size(736, 395);
+            this.TableGrid.TabIndex = 1;
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(761, 450);
+            this.Controls.Add(this.TableGrid);
+            this.Controls.Add(this.AddRowButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "main";
+            this.Text = "Таблица";
+            ((System.ComponentModel.ISupportInitialize)(this.TableGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button AddRowButton;
+        private System.Windows.Forms.DataGridView TableGrid;
+    }
+}
+
