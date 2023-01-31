49d628c07c40dbe73fb95ce6c1e206687d559583
ValeriyaBogacheva <117594694+ValeriyaBogacheva@users.noreply.github.com>
2023-01-31 20:20:25 +0300

    Add files via upload

---
+++ 4.2. CreatingDataTable/main.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CreatingDataTable
+{
+    public partial class main : Form
+    {
+        private DataTable CustomersTable = new DataTable("Customers");
+        public main()
+        {
+           
+
+            InitializeComponent();
+            TableGrid.DataSource = CustomersTable;
+            CustomersTable.Columns.Add("CustomerID", Type.GetType("System.String"));
+            CustomersTable.Columns.Add("CompanyName", Type.GetType("System.String"));
+            CustomersTable.Columns.Add("ContactName", Type.GetType("System.String"));
+            CustomersTable.Columns.Add("ContactTitle", Type.GetType("System.String"));
+            CustomersTable.Columns.Add("Address", Type.GetType("System.String"));
+            CustomersTable.Columns.Add("City", Type.GetType("System.String"));
+            CustomersTable.Columns.Add("Country", Type.GetType("System.String"));
+            CustomersTable.Columns.Add("Phone", Type.GetType("System.String"));
+
+            //DataColumn[] KeyColumns = new DataColumn[1];
+            //KeyColumns[0] = CustomersTable.Columns["CustomerID"];
+            //CustomersTable.PrimaryKey = KeyColumns;
+
+            CustomersTable.Columns["CustomerID"].AllowDBNull = false;
+            CustomersTable.Columns["CompanyName"].AllowDBNull = false;
+        }
+
+        private void AddRowButton_Click(object sender, EventArgs e)
+        {
+            try{
+                DataRow CustRow = CustomersTable.NewRow();
+                Object[] CustRecord =  {"ALFKI", "Alfreds Futterkiste", "Maria Anders",
+                "Sales Representative", "Obere Str. 57", "Berlin",
+                   "Germany", "030-0076545"};
+                CustRow.ItemArray = CustRecord;
+                CustomersTable.Rows.Add(CustRow);
+            }
+            catch (DataException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
